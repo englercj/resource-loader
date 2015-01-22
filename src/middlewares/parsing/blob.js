23530a73a3162d120e55d507ef27aa89ce8f4174
Chad Engler <englercj@live.com>
2015-01-22 09:18:16 -0800

    add blob parser

---
+++ src/middlewares/parsing/blob.js
@@ -0,0 +1,24 @@
+var URL = window.URL || window.webkitURL;
+
+// a middleware for transforming XHR loaded Blobs into more useful objects
+
+module.exports = function () {
+    return function (resource, next) {
+        // if this was an XHR load
+        if (resource.xhr && resource.xhrType === Resource.XHR_RESPONSE_TYPE.BLOB) {
+            // if content type says this is an image, then we need to transform the blob into an Image object
+            if (resource.data.type.indexOf('image') === 0) {
+                resource.data = new Image();
+                resource.data.src = URL.createObjectURL(request.data);
+
+                // cleanup the no longer used blob after the image loads
+                resource.data.onload = function () {
+                    URL.revokeObjectURL(resource.data.src);
+                    resource.data.onload = null;
+                };
+            }
+        }
+
+        next();
+    };
+};
