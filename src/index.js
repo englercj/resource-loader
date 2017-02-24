8e39cad1960e20dc1deab36e33641a035c333f4e
Chad Engler <englercj@live.com>
2017-02-24 09:18:42 -0800

    attempted fix for #84

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import Loader from './Loader';
-import Resource from './Resource';
-import * as async from './async';
-import * as b64 from './b64';
+// import Loader from './Loader';
+// import Resource from './Resource';
+// import * as async from './async';
+// import * as b64 from './b64';
+
+/* eslint-disable no-undef */
+
+const Loader = require('./Loader').default;
+const Resource = require('./Resource').default;
+const async = require('./async');
+const b64 = require('./b64');
 
 Loader.Resource = Resource;
 Loader.async = async;
 Loader.base64 = b64;
 
 // export manually, and also as default
-module.exports = Loader; // eslint-disable-line no-undef
-export default Loader;
+module.exports = Loader;
+// export default Loader;
+module.exports.default = Loader;
