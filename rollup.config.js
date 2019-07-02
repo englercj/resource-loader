c879c344d9160172371df57c241472b4bf3a0dec
Matt Karl <matt@mattkarl.com>
2019-07-02 06:57:40 -0700

    Enables transpiling for output builds (#133)
    * WIP
    
    * Support bundle, clearning typings
    
    * Adds watch command
    
    * Fixes export test
    
    * Updates typings to add signals import
    
    * Clean up build commands for Travis
    
    * Remove gulp references from readme
    
    * Bumped sinon, mocha and karma to latest versions
    
    * Re-enables support for babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const plugins = [
     resolve(),
-    commonjs()
+    commonjs(),
+    babel()
 ];
 const sourcemap = true;
 const freeze = false;
