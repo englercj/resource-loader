08b70be86d3e76067d85cc4c94b60f7bbf6e6131
Chad Engler <englercj@live.com>
2020-03-07 14:54:35 -0800

    Add some missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-export { AbstractLoadStrategy } from './load_strategies/AbstractLoadStrategy';
+export { AbstractLoadStrategy, ILoadConfig } from './load_strategies/AbstractLoadStrategy';
 export { AudioLoadStrategy } from './load_strategies/AudioLoadStrategy';
-export { ImageLoadStrategy } from './load_strategies/ImageLoadStrategy';
-export { MediaElementLoadStrategy } from './load_strategies/MediaElementLoadStrategy';
+export { ImageLoadStrategy, IImageLoadConfig } from './load_strategies/ImageLoadStrategy';
+export { MediaElementLoadStrategy, IMediaElementLoadConfig } from './load_strategies/MediaElementLoadStrategy';
 export { VideoLoadStrategy } from './load_strategies/VideoLoadStrategy';
-export { XhrLoadStrategy } from './load_strategies/XhrLoadStrategy';
+export { XhrLoadStrategy , XhrResponseType, IXhrLoadConfig } from './load_strategies/XhrLoadStrategy';
 
-export { Loader } from './Loader';
-export { Resource } from './Resource';
+export { Loader, IAddOptions } from './Loader';
+export { Resource, IResourceOptions } from './Resource';
 export { ResourceType, ResourceState } from './resource_type';
