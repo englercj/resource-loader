c8916151491a90fa80fd4242a71e7314f68524dd
Chad Engler <englercj@live.com>
2020-03-07 15:16:38 -0800

    Update dev deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const plugins = [
-    typescript({
-        typescript: require('typescript'),
-        tsconfig: './tsconfig.json',
-    }),
+    typescript(),
     resolve(),
     commonjs(),
 ];
